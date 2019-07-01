ccccd8c0f692a0376fdb7c9229813dea72485941
Frezyx <40857927+Frezyx@users.noreply.github.com>
2019-07-01 23:13:40 +0200

    Изменения для направлений

--- napravlen/resize.js
+++ napravlen/resize.js
@@ -13,4 +13,11 @@ $(function(){
       $('.resize-img').height($('.resize-img').width()/3);
     });
   });
-   
+
+  $(function(){
+    $('.img-bok').height($('.img-bok').width()/3);
+  
+    $(window).resize(function(){
+      $('.img-bok').height($('.img-bok').width()/3);
+    });
+  });
